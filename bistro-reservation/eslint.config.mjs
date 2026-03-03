10d2a829dfaec0e490966fe8b33d364b8313437f
miyamibu <jnuluo@gmail.com>
2026-03-04 01:04:52 +0900

    chore: remove deprecated lint and image deps

---
+++ bistro-reservation/eslint.config.mjs
@@ -0,0 +1,25 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const config = [
+  {
+    ignores: [".next/**", "coverage/**", "next-env.d.ts", "node_modules/**", "out/**"],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": "error",
+    },
+  },
+];
+
+export default config;
